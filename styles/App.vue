31ccc4be4141b5e72a3dcee142085f328efe474f
RetaxMaster <retaxmaster@hotmail.com>
2021-04-27 17:03:00 -0500

    Adicion de las plantillas de estilos

---
+++ styles/App.vue
@@ -0,0 +1,57 @@
+<template>
+    
+    Este archivo es únicamente para que puedas copiar la etiqueta style, este archivo no influye en nada en nuestro proyecto.
+
+    PD. No copies esta etiqueta template, esta aquí solo para avisarte de esto :D
+
+</template>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.dashboard {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 20px;
+
+  background: rgb(154,221,212);
+  background: linear-gradient(114deg, rgba(154,221,212,1) 0%, rgba(171,214,227,1) 100%);
+}
+
+.image-container {
+  display: flex;
+}
+
+.image-container img {
+  display: inline-block;
+  width: 100%;
+}
+
+.selected-card {
+  display: flex;
+  justify-content: center;
+}
+
+.options {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 2rem;
+  gap: 15px;
+  justify-items: center;
+}
+
+.options .card {
+  cursor: pointer;
+}
+
+</style>
---
+++ styles/Card.vue
@@ -0,0 +1,52 @@
+<template>
+    
+    Este archivo es únicamente para que puedas copiar la etiqueta style, este archivo no influye en nada en nuestro proyecto.
+
+    PD. No copies esta etiqueta template, esta aquí solo para avisarte de esto :D
+
+</template>
+
+<style scoped>
+
+.card {
+    max-width: 300px;
+    position: relative;
+
+    transition: all ease 0.5s;
+    transform-style: preserve-3d;
+    transform: perspective(600px) rotateY(0deg);
+
+}
+
+.card .front {
+    padding: 15px;
+    border-radius: 5px;
+    box-shadow: 0 0 4px 0 rgb(0 0 0 / 8%), 0 2px 4px 0 rgb(0 0 0 / 12%);
+    border-radius: 3px;
+    background: #fff;
+    backface-visibility: hidden;
+}
+
+.card::before {
+    content: '';
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: #000;
+    border-radius: 5px;
+    z-index: -1;
+}
+
+.card.closed {
+    transform: perspective(600px) rotateY(180deg);
+}
+
+.card h2 {
+    text-align: center;
+    margin-bottom: 10px;
+}
+
+</style>
